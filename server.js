dd53fffa9f48f8c3d2f6784dd7d975d2a19e0cd9
Michal Raczkowski <michal.raczkowski.88@gmail.com>
2018-10-18 19:37:59 +0100

    App

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.get('/auth/google', function(req, res){
+    res.render('app');
+});
+
+app.get('/userform', function (req, res) {
+    res.render('hello', {
+        user:
+            {first_name: req.query.first_name, last_name: req.query.last_name}
+    });
+});
+
+
+
+
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
